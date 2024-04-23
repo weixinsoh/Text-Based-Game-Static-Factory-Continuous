a5b978ce2dc8d75d63fa6d04d96178d1b16f92fb
schi0088 <schi0088@student.monash.edu>
2024-04-23 17:34:40 +1000

    Created scrap classes Jar Of Pickles and Pot of Gold. Modified Puddle to allow it be consumable by the player.

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -2,15 +2,30 @@ package game.grounds;
 
 import edu.monash.fit2099.engine.positions.Ground;
 
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import game.consumable.Consumable;
+
+import static edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations.INCREASE;
+import static edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes.HEALTH;
+
 /**
  * Class representing puddle.
  */
-public class Puddle extends Ground {
+public class Puddle extends Ground implements Consumable {
+
+    private static final int PUDDLE_HEALTH = 1;
 
     /**
      * Constructor of the Puddle class.
      */
     public Puddle() {
         super('~');
+    }
+
+    public String consumedBy(Actor actor){
+        actor.modifyAttribute(HEALTH, INCREASE, PUDDLE_HEALTH);
+        return "Intern slurps from puddle and increase health by 1.";
     }
 }
---
+++ src/game/scraps/PotOfGold.java
@@ -0,0 +1,19 @@
+package game.scraps;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import game.consumable.ConsumableItem;
+
+public class PotOfGold extends ConsumableItem {
+
+    private static final int POT_OF_GOLD_BALANCE = 10;
+    public PotOfGold() {
+        super("Pot of Gold", '$', true);
+    }
+
+    @Override
+    public String consumedBy(Actor actor) {
+        actor.addBalance(POT_OF_GOLD_BALANCE);
+        return "Pot of Gold is placed into wallet for 10 gold.";
+    }
+}
+
