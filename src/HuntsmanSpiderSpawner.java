f9598182d17869f7e64a70bbd7c59c50742bc065
Jeevan <jvet0005@student.monash.edu>
2024-04-30 15:16:50 +1000

    Created HuntsmanSpiderSpawner interface (implements Spawncer)

---
+++ src/HuntsmanSpiderSpawner.java
@@ -0,0 +1,13 @@
+import edu.monash.fit2099.engine.positions.Location;
+import game.Spawner;
+import game.actors.creatures.HuntsmanSpider;
+
+public class HuntsmanSpiderSpawner implements Spawner {
+
+    @Override
+    public void spawn(Location location) {
+        if (Math.random() <= 0.05) {
+            new HuntsmanSpider();
+        }
+    }
+}
