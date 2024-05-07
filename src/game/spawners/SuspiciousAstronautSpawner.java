6ccc4669e28999d63edf62b8d66dff150c06f09c
Jeevan <jvet0005@student.monash.edu>
2024-05-07 15:37:10 +1000

    Created SuspiciousAstronautSpawner

---
+++ src/game/spawners/SuspiciousAstronautSpawner.java
@@ -0,0 +1,16 @@
+package game.spawners;
+
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.creatures.AlienBug;
+import game.actors.creatures.SuspiciousAstronaut;
+
+public class SuspiciousAstronautSpawner implements Spawner{
+    public SuspiciousAstronautSpawner(){}
+    @Override
+    public void spawn(Location location) {
+        if (Math.random() <= 0.05) {            //5% chance of spawning
+            SuspiciousAstronaut suspiciousAstronaut = new SuspiciousAstronaut();
+            location.addActor(suspiciousAstronaut);
+        }
+    }
+}
