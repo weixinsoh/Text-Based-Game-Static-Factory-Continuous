62f227fed13cbd48af54da3a6590b2f3132762f4
Jeevan <jvet0005@student.monash.edu>
2024-05-07 15:36:14 +1000

    Altered implementation of AlienBugSpawner

--- src/game/spawners/AlienBugSpawner.java
+++ src/game/spawners/AlienBugSpawner.java
@@ -2,13 +2,15 @@ package game.spawners;
 
 import edu.monash.fit2099.engine.positions.Location;
 import game.actors.creatures.AlienBug;
-import game.actors.creatures.HuntsmanSpider;
 
 public class AlienBugSpawner implements Spawner{
+    public AlienBugSpawner(){}
     @Override
     public void spawn(Location location) {
-        if (Math.random() <= 0.1) {
-            new AlienBug();
+        if (Math.random() <= 0.10) {            //10% chance of spawning
+            AlienBug alienBug = new AlienBug();
+            location.addActor(alienBug);
+        }
+
         }
     }
-}
