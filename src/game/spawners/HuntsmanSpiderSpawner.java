4da89e7ea51ad05e971240f404457f890223812d
Jeevan <jvet0005@student.monash.edu>
2024-05-07 15:36:59 +1000

    Altered method of HuntsmanSpiderSpawner to utilise location passed in from Crater

--- src/game/spawners/HuntsmanSpiderSpawner.java
+++ src/game/spawners/HuntsmanSpiderSpawner.java
@@ -1,15 +1,17 @@
 package game.spawners;
 
 import edu.monash.fit2099.engine.positions.Location;
-import game.spawners.Spawner;
 import game.actors.creatures.HuntsmanSpider;
 
-public class HuntsmanSpiderSpawner implements Spawner {
 
+public class HuntsmanSpiderSpawner implements Spawner {
+    public HuntsmanSpiderSpawner(){}
     @Override
     public void spawn(Location location) {
-        if (Math.random() <= 0.05) {
-            new HuntsmanSpider();
+        if (Math.random() <= 0.05) {                //5% chance of spawning
+            HuntsmanSpider hunstmanSpider = new HuntsmanSpider();
+            location.addActor(hunstmanSpider);
         }
+
     }
 }
