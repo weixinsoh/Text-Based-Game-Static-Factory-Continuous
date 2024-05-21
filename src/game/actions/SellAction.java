164325028c7ef78d707394c3a109c1ba744b6641
Jeevan <jvet0005@student.monash.edu>
2024-05-21 15:49:20 +1000

    Created SellAction Class

---
+++ src/game/actions/SellAction.java
@@ -0,0 +1,20 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.scraps.Sellable;
+
+public class SellAction extends Action {
+
+    private Sellable sellableItem;
+
+    public SellAction(Sellable sellableItem){
+        this.sellableItem = sellableItem;
+    }
+    @Override
+    public String execute(Actor actor, GameMap map) {return null;}
+
+    @Override
+    public String menuDescription(Actor actor){return null;}
+}
