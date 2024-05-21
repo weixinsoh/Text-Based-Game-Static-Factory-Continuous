84a187eccda3e475b7dbb44597a4876f630f5ca3
weixin <wsoh0009@student.monash.edu>
2024-05-21 15:14:00 +1000

    Added javadoc documentation

--- src/game/Utility.java
+++ src/game/Utility.java
@@ -2,6 +2,11 @@ package game;
 import java.util.Random;
 
 public class Utility {
+    /**
+     * This function is used to generate random integer in a given range.
+     * @param lower the lower bound
+     * @param upper the upper bound
+     */
     public static int generateRandomInt(int lower, int upper) {
         return lower + new Random().nextInt(upper-lower);
     }
