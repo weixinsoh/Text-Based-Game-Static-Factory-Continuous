9e53f27c444c4a49f2f05aad6a77246ce298a654
YingKhor <ykho0020@student.monash.edu>
2024-05-21 16:49:06 +1000

    Create Astley class, implementing Purchasable interface to be purchased from the computer terminal

--- src/game/scraps/specialscraps/Astley.java
+++ src/game/scraps/specialscraps/Astley.java
@@ -3,6 +3,7 @@ package game.scraps.specialscraps;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.Location;
 import game.actions.ListenAction;
@@ -14,65 +15,55 @@ public class Astley extends Item implements Purchasable, Monologuer {
 
     private static final int CREDIT = 50;
 
-    private boolean isSubscribed;
-
-    /**
-     * Count that keeps track of the time in the item inventory
-     */
-    private int count = 0;
-
-    /**
-     * Integer that keeps tracks of the amount of ticks still required before the tree matures
-     */
     private static final int TICKS_TO_PAY_SUBSCRIPTION = 5;
 
     private static final int CREDIT_TO_PAY_SUBSCRIPTION = 1;
+
+    private boolean isSubscribed = false;
+
+    private int count = 0;
 
     public Astley() {
         super("Astley", 'z', true);
     }
 
-    public void tick(Location currentLocation, Actor actor) { // if not enough balance, then stop subscribe, then if next tick got balance le, tick directly start again??
-        this.count++;
-        if (this.count % TICKS_TO_PAY_SUBSCRIPTION == 0)
-            System.out.println(this.pay_subscription(actor));
-    }
+//    @Override
+//    public void tick(Location currentLocation, Actor actor) {
+//        Display display = new Display();
+//        count++;
+//        if (this.count % TICKS_TO_PAY_SUBSCRIPTION == 0) {
+//            if (actor.getBalance() >= CREDIT_TO_PAY_SUBSCRIPTION){
+//                actor.deductBalance(CREDIT_TO_PAY_SUBSCRIPTION);
+//                display.println("Subscription payment received!");
+//            } else {
+//                isSubscribed = false;
+//                display.println("You don't have enough money to continue using our AI services. Goodbye.");
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public ActionList allowableActions(Actor owner){
+//        ActionList actions = new ActionList();
+//        if (isSubscribed)
+//            actions.add(new ListenAction(this));
+//        return actions;
+//    }
 
-    public String pay_subscription(Actor actor) { // When purchased the AI Device from computer terminal, is it directly subscribed
-
-        if (actor.getBalance() >= CREDIT_TO_PAY_SUBSCRIPTION) {
-            isSubscribed = true;
-            actor.deductBalance(CREDIT_TO_PAY_SUBSCRIPTION);
-            return "Subscription payment received!";
-        } else {
-            isSubscribed = false;
-            this.count = 0;
-            return "You don't have enough money to continue using our AI services. Goodbye.";
-        }
-    }
-
-    @Override
-    public ActionList allowableActions(Actor owner){
-        ActionList actions = new ActionList();
-        if (isSubscribed)
-            actions.add(new ListenAction(this));
-        return actions;
-    }
-
-    @Override
-    public List<String> allowableMonologues(Actor owner){
-        List<String> monologues = new ArrayList<>();
-        monologues.add("The factory will never gonna give you up, valuable intern!");
-        monologues.add("We promise we never gonna let you down with a range of staff benefits.");
-        monologues.add("We never gonna run around and desert you, dear intern!");
-        if (owner.getItemInventory().size() > 10)
-            monologues.add("We never gonna make you cry with unfair compensation.");
-        if (owner.getBalance() > 50)
-            monologues.add("Trust is essential in this business. We promise we never gonna say goodbye to a valuable intern like you.");
-        if (owner.getAttribute(BaseActorAttributes.HEALTH) < 2)
-            monologues.add("Don't worry, we never gonna tell a lie and hurt you, unlike those hostile creatures.");
-        return monologues;
-    }
+//    @Override
+//    public List<String> allowableMonologues(Actor owner){
+//        List<String> monologues = new ArrayList<>();
+//        monologues.add("The factory will never gonna give you up, valuable intern!");
+//        monologues.add("We promise we never gonna let you down with a range of staff benefits.");
+//        monologues.add("We never gonna run around and desert you, dear intern!");
+//        if (owner.getItemInventory().size() > 10)
+//            monologues.add("We never gonna make you cry with unfair compensation.");
+//        if (owner.getBalance() > 50)
+//            monologues.add("Trust is essential in this business. We promise we never gonna say goodbye to a valuable intern like you.");
+//        if (owner.getAttribute(BaseActorAttributes.HEALTH) < 2)
+//            monologues.add("Don't worry, we never gonna tell a lie and hurt you, unlike those hostile creatures.");
+//        return monologues;
+//    }
 
     @Override
     public String purchase(Actor actor) {
