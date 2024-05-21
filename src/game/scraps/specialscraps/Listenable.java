74224487066fb539fea0e05296ddc3518dd9e710
YingKhor <ykho0020@student.monash.edu>
2024-05-21 16:51:36 +1000

    Create a monologuer interface and implemented by astley to store monologue options

--- src/game/scraps/specialscraps/Listenable.java
+++
@@ -1,4 +0,0 @@
-package game.scraps.specialscraps;
-
-public interface Listenable {
-}
--- src/game/scraps/specialscraps/Monologuer.java
+++ src/game/scraps/specialscraps/Monologuer.java
@@ -4,7 +4,17 @@ import edu.monash.fit2099.engine.actors.Actor;
 
 import java.util.List;
 
+/**
+ * Interface representing monologuer.
+ *
+ */
 public interface Monologuer {
 
+    /**
+     * Returning a list of monologue options that can be performed when met certain conditions.
+     *
+     * @param owner The actor who listen to the monologuer.
+     * @return a list of string containing available monologue options.
+     */
     List<String> allowableMonologues(Actor owner);
 }
