8374dc2bba0a01c86ab1b5acbc549b812a178d7b
Jeevan <jvet0005@student.monash.edu>
2024-05-22 00:33:20 +1000

    Altered JarOfPickles so it can be sold for 25 credits, but 50% chance sold for 50 credits

--- src/game/scraps/specialscraps/JarOfPickles.java
+++ src/game/scraps/specialscraps/JarOfPickles.java
@@ -4,13 +4,17 @@ package game.scraps.specialscraps;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Location;
 import game.actions.ConsumeAction;
+import game.actions.SellAction;
+import game.capabilities.Status;
+import game.scraps.Sellable;
 
 /**
  * Class that represents a Jar Of Pickles that can be consumed to randomly hurt or heal the player
  *
  */
-public class JarOfPickles extends Item implements Consumable {
+public class JarOfPickles extends Item implements Consumable, Sellable {
 
     private static final int JAR_OF_PICKLES_HEALTH_AFFECTED = 1;
 
@@ -52,10 +56,28 @@ public class JarOfPickles extends Item implements Consumable {
      * @see Item#allowableActions(Actor)
      * @return a list of actions that can be performed on the consumable item Jar Of Pickles
      */
+
     @Override
-    public ActionList allowableActions(Actor owner) {
+    public ActionList allowableActions(Actor otherActor, Location location){
         ActionList actions = new ActionList();
         actions.add(new ConsumeAction(this));
+        if(otherActor.hasCapability(Status.BUYER)){
+            actions.add(new SellAction(this));
+        }
         return actions;
+    }
+
+    @Override
+    public int getCreditForSale() {
+        if (Math.random() <= 0.5){
+            return 50;
+        } else {
+            return 25;
+        }
+    }
+
+    @Override
+    public Item soldBy() {
+        return this;
     }
 }
