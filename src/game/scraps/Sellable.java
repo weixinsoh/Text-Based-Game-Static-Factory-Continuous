2e9f782b7701f31ffa72f23ab101bc2c073d5f5a
Jeevan <jvet0005@student.monash.edu>
2024-05-30 14:11:54 +1000

    Consolidated methods in Sellable to create sell() method

--- src/game/scraps/Sellable.java
+++ src/game/scraps/Sellable.java
@@ -3,6 +3,5 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 
 public interface Sellable {
-    int getCreditForSale();
-    Item soldBy();
+    String sell(Actor otherActor);
 }
