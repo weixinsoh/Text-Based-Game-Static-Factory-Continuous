db4467af709e104bbbb2471777d7c84de0b0546f
schi0088 <schi0088@student.monash.edu>
2024-06-02 21:54:05 +1000

    Delete unwanted import

--- src/game/scraps/LargeBolt.java
+++ src/game/scraps/LargeBolt.java
@@ -2,7 +2,6 @@ package game.scraps;
 
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
